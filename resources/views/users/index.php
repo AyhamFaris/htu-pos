d42093d2cb84028b579b3320e11df8f5235a4a3c
your_name <ayhamfaris1@gmail.com>
2023-01-01 17:44:00 +0200

    dd

--- resources/views/users/index.php
+++ resources/views/users/index.php
@@ -16,7 +16,7 @@
                 <tr>
                     <td class="text-center">
                         <div class="d-flex align-items-center">
-                            <img src="<?=$_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST']?>/resources/Images/?>/resources/Images/<?= $user->img ?>" alt="" style="width: 45px; height: 45px" class="rounded-circle" />
+                            <img src="<?=$_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST']?>/resources/Images/<?= $user->img ?>" alt="" style="width: 45px; height: 45px" class="rounded-circle" />
                             <div class="ms-3">
                                 <p class="fw-bold mb-1"><?= $user->display_name ?></p>
                             </div>
