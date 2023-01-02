a956f829302db7ab89c8b33927140e4ff7ebc27f
your_name <ayhamfaris1@gmail.com>
2023-01-02 12:34:33 +0200

    commit

--- resources/views/login.php
+++ resources/views/login.php
@@ -6,7 +6,7 @@
           <form action="/authenticate" method="POST">
             <h1>SIGN IN</h1>
             <input type="text" name="username" placeholder="Your Name">
-            <input type="text" name="password" placeholder="Your Password">
+            <input type="password" name="password" placeholder="Your Password">
             <label for="vehicle1">Remeber me</label>
             <input type="checkbox" id="vehicle1" name="remember_me" value="Bike">
             <button class="submit">LOGIN</button>
