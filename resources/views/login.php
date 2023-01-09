4d219671e3a29a450474c86cc61c3a7b17660c14
your_name <ayhamfaris1@gmail.com>
2023-01-09 20:22:06 +0200

    commit

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,23 +1,32 @@
-
-<section class="login">
-    <div class="login_box">
-      <div class="left">
-        <div class="contact">
-          <form action="/authenticate" method="POST">
-            <h1>SIGN IN</h1>
-            <input type="text" name="username" placeholder="Your Name">
-            <input type="password" name="password" placeholder="Your Password">
-            <label for="vehicle1">Remeber me</label>
-            <input type="checkbox" id="vehicle1" name="remember_me" value="Bike">
-            <button class="submit">LOGIN</button>
-          </form>
+<div class="container">
+  <div class="screen">
+    <div class="screen__content">
+      <form class="login" action="/authenticate" method="POST">
+        <div class="login__field">
+          <i class="login__icon fas fa-user"></i>
+          <input type="text" name="username" class="login__input" placeholder="User name">
         </div>
-      </div>
-      <div class="right">
-        <div class="right-text">
-          <h2>HTU POS SYSTEM</h2>
-          <h5>Al-Hussein Technical University</h5>
+        <div class="login__field">
+          <i class="login__icon fas fa-lock"></i>
+          <input type="password" name="password" class="login__input" placeholder="Password">
+        </div>
+        <button class="button login__submit">
+          <span class="button__text">Log In Now</span>
+          <i class="button__icon fas fa-chevron-right"></i>
+        </button>
+      </form>
+      <div class="social-login">
+        <div class="social-icons">
+          <label for="vehicle1">Remeber me</label>
+          <input type="checkbox" id="vehicle1" name="remember_me" value="Bike">
         </div>
       </div>
     </div>
-  </section>
+    <div class="screen__background">
+      <span class="screen__background__shape screen__background__shape4"></span>
+      <span class="screen__background__shape screen__background__shape3"></span>
+      <span class="screen__background__shape screen__background__shape2"></span>
+      <span class="screen__background__shape screen__background__shape1"></span>
+    </div>
+  </div>
+</div>
